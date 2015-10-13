1a76852289c821d87e1fae70928558e962ba84ad
KesavaEgenTraining <movetojunk@gmail.com>
2015-10-13 12:35:51 -0500

    feat(master): add angular module creation and controller creation

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,23 @@
 /**
  * Created by kesava on 10/11/2015.
  */
+
+(function()
+{
+    'use strict';
+
+    //Declare module
+    angular.module('moduleName',[]);
+
+    //Declare controller in module in which you want
+    angular.module('moduleName').controller('controllerName',controllerNameFunctionName);
+
+    //Define controller
+    function controllerNameFunctionName()
+    {
+        this.var1 = "...";
+        this.var2 = "...";
+        var var3 = "...";
+    }
+
+})();
